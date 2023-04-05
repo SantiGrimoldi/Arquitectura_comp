e2ed27d2c195c291650b1bac9829bd757a527bc7
zartf <tomas.alcazar@ing.austral.edu.ar>
2023-04-04 22:50:27 -0300

    testBinaryToAscii & testAsciiToBinary

--- main/TP1/Ascii.java
+++ main/TP1/Ascii.java
@@ -5,11 +5,29 @@ public class Ascii implements AsciiEncoder{
     @Override
     public String encode(String binary) {
         String result = "";
-        for (int i = 0; i <= binary.length() - 1 ; i += 8) {
-            String group = binary.substring(i, Math.min(i + 8, binary.length()));
-            int decimal = Integer.parseInt(group, 2);
+        int decimal = 0;
+        int bitValue = 128;
+        int bitCount = 0;
+
+        for (int i = 0; i < binary.length(); i++) {
+            int bit = binary.charAt(i) - '0';
+            decimal += bit * bitValue;
+            bitValue /= 2;
+            bitCount++;
+
+            if (bitCount == 8) {
+                result += (char) decimal;
+                decimal = 0;
+                bitValue = 128;
+                bitCount = 0;
+            }
+        }
+
+        if (bitCount > 0) {
+            decimal += (bitValue - 1);
             result += (char) decimal;
         }
+
         return result;
     }
 
